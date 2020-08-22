ca841f9b021d441d763447ff0adebd9daa3a1cf9
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-08-22 13:04:12 -0400

    Selecting a currency other than USD

--- coincap_global.py
+++ coincap_global.py
@@ -2,12 +2,13 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest'
+currency = 'JPY'
 
 parameters = {
-  'start':'1',
-  'limit':'1',
-  'convert':'USD'
+#   'start':'1',
+#   'limit':'1',
+  'convert': currency
 }
 headers = {
   'Accepts': 'application/json',
@@ -20,16 +21,21 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   results = response.json()
-  #data = json.dumps(results, sort_keys=True, indent=4)
-  #print(data)
+#   data = json.dumps(results, sort_keys=True, indent=4)
+#   print(data)
 
-  slug = results['data'][0]['slug']
-  symbol = results['data'][0]['symbol']
-  total_supply = results['data'][0]['total_supply']
-
-  print("Slug: {}".format(slug))
-  print("Symbol: {}".format(symbol))
-  print("Total Supply: {}".format(total_supply))
+  active_cryptocurrencies = results['data']['active_cryptocurrencies']
+  active_market_pairs = results['data']['active_market_pairs']
+  last_updated = results['data']['last_updated']
+  total_market_cap = results['data']['quote'][currency]['total_market_cap']  
+  total_volume_24h = results['data']['quote'][currency]['total_volume_24h']
+  #total_cryptocurrencies = results['data']['total_cryptocurrencies']
   
+
+  print("There are currently {} active cryptocurrencies and {} active pairs markets.".format(active_cryptocurrencies, active_market_pairs))
+  print("The global cap of all cryptos is {} and the 24th global volume is {}".format(total_market_cap, total_volume_24h))
+  print()
+  print("This information was last updated on {}".format(last_updated))
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
